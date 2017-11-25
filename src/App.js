9a14cd631461f137560a3bbe6852356952756050
unknown <linkmagic@gmail.com>
2017-11-26 00:26:32 +0200

    design style of publication on the main board

--- src/App.js
+++ src/App.js
@@ -20,12 +20,22 @@ function HeaderPanelLogin() {
   return (
     <div className="header-panel-login">
       <p className="login-user-name">User Name</p>
-      <img src="user.png" alt="user" />
-      <div class="header-panel-login-menu">
+      <img src="img/user.png" alt="user" />
+      <div className="header-panel-login-menu">
         <p><a href="#">Login</a></p>
         <p><a href="#">Logout</a></p>
         <p><a href="#">Settings</a></p>
       </div>
+    </div>
+  );
+}
+
+function PostRating() {
+  return (
+    <div className="post-item-rating">
+      <img src="img/rating-down.png" alt="rating-down"/>
+      <p>+107</p>
+      <img src="img/rating-up.png" alt="rating-up"/>
     </div>
   );
 }
@@ -48,16 +58,27 @@ class App extends Component {
         </header>
         <div className="content">
           <WhiteSpaceTop/>
-          <p>
-            line<br/>
-            line<br/>
-            line<br/>
-            line<br/>
-            line<br/>
-            line<br/>
-            line<br/>
-            line<br/>
-          </p>
+          <div>
+            <div className="posts-board">
+
+              <div className="post-item">
+                <h3 className="post-item-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+                <div className="post-item-info-short">
+                  <p className="post-item-date">2017.10.10 15:50</p>
+                  <a href="#" className="post-item-author">Anatoly Vaskin</a>
+                </div>
+                <p className="post-item-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid architecto atque beatae earum enim error, impedit in ipsam laboriosam non odit possimus quas quia quibusdam soluta ullam, vel, voluptatem! consectetur adipisicing elit. Accusantium aliquid architecto atque beatae earum enim error, impedit in ipsam laboriosam non odit possimus quas quia quibusdam soluta ullam, vel, voluptatem!</p>
+                <div className="post-item-info-long">
+                  <a href="#" className="post-item-comments">10 comments</a>
+                  <PostRating/>
+                  <a href="#" className="post-item-bookmark-it">Bkmrk</a>
+                </div>
+              </div>
+
+
+
+            </div>
+          </div>
         </div>
       </div>
     );
